1285230cf03b585132b0a67f9729528427c146c8
jay <jaysoni2410@gmail.com>
2023-06-07 02:03:55 +0200

    Exercise 1

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,8 @@
+const MAX_MISSILES: i32 = 8;
+const READY_AMOUNT: i32 = 2;
 fn main() {
-    println!("Hello, world!");
+    let (missiles, ready): (i32, i32) = (MAX_MISSILES, READY_AMOUNT);
+    println!("Firing {} of my {} missiles...", ready, missiles);
+    println!("{} missiles left", missiles-ready);
+
 }
